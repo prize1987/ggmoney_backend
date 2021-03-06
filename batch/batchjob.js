fe6b08e7e2486095abd963c992d4d30bfdc5efd4
강정은 <je@gangjeong-eun-ui-MacBookPro.local>
2021-03-06 23:15:48 +0900

    ok

--- batch/batchjob.js
+++ batch/batchjob.js
@@ -4,10 +4,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const connection = mysql.createConnection({
-  host: process.env.HOST,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 connection.connect();
