7d5c197751d1386b084bfc3397ee88ac6853e576
강정은 <je@gangjeong-eun-ui-MacBookPro.local>
2021-03-07 17:59:04 +0900

    api test

--- batch/batchjob.js
+++ batch/batchjob.js
@@ -18,3 +18,19 @@ connection.query('SELECT * from STORE_INFO', (error, rows, fields) => {
 });
 
 connection.end();
+
+const apiTest = () => {
+  const url = 'https://openapi.gg.go.kr/RegionMnyFacltStus';
+  const appKey = process.env.API_KEY_GGMONEY;
+  const pIndex = 1;
+  const pSize = 10;
+  const type = 'json';
+
+  let fetch_url = url + '?KEY=' + appKey;
+  fetch_url += '&pIndex=' + pIndex;
+  fetch_url += '&pSize=' + pSize;
+  fetch_url += '&type=' + type;
+  fetch_url += '&SIGUN_NM=' + sigun;
+};
+
+apiTest();
