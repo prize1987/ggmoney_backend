fae7c1e381f189775917e6141f9b43ae28d8e410
강정은 <je@gangjeong-eun-ui-MacBookPro.local>
2021-03-07 21:38:53 +0900

    adjust file

--- batch/batchjob.js
+++ batch/batchjob.js
@@ -1,17 +1,46 @@
 import * as api from './api.js';
 import database from './database.js';
 
-const main = async () => {
-  const data = await api.getSigunData(1, 10, '수원시');
-  console.log(data);
+const db = new database();
 
-  const db = new database();
+const insertStoreInfo = async (data) => {
   db.connect();
 
-  const tableData = await db.select('select * FROM STORE_INFO');
-  console.log(tableData);
+  const query = `INSERT INTO STORE_INFO (CMPNM_NM,
+    INDUTYPE_NM,
+    TELNO,
+    REFINE_LOTNO_ADDR,
+    REFINE_ROADNM_ADDR,
+    REFINE_ZIP_CD,
+    REFINE_WGS84_LOGT,
+    REFINE_WGS84_LAT,
+    SIGUN_NM) VALUES ?`;
+
+  const result = await db.insert(query, data);
+  console.log(result);
 
   db.end();
+};
+
+const getSigunData = async (pIndex, pSize, pSigun) => {
+  const data = await api.getSigunData(pIndex, pSize, pSigun);
+  return data.map((row) => [
+    row.CMPNM_NM,
+    row.INDUTYPE_NM,
+    row.TELNO,
+    row.REFINE_LOTNO_ADDR,
+    row.REFINE_ROADNM_ADDR,
+    row.REFINE_ZIP_CD,
+    row.REFINE_WGS84_LOGT,
+    row.REFINE_WGS84_LAT,
+    row.SIGUN_NM,
+  ]);
+};
+
+const main = async () => {
+  const data = await getSigunData(1, 10, '수원시');
+
+  insertStoreInfo(data);
 };
 
 main();
